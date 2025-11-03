855a79a08986cef50c799e0702633ad7077be549
WeonJae <kongwonjae123@naver.com>
2025-11-03 11:52:59 +0900

    Initial commit for Kong branch

---
+++ src/test/java/org/likelion/hsu/db_project/DbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.likelion.hsu.db_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
